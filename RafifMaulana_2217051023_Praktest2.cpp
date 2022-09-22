fb6236aac736d687da77a74ef2aad7369045af3a
RAFIFMAULANA <112968842+RAFIFMAULANA@users.noreply.github.com>
2022-09-22 01:43:02 -0700

    Add files via upload

---
+++ RafifMaulana_2217051023_Praktest2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int angka;
+cin >> angka;
+
+if (angka==1){
+	cout<< "satu\n";
+}else if (angka==2){
+	cout << "dua\n";
+}else if (angka==3){
+	cout << "tiga\n";
+}else{
+	cout << "Bilangan lebih besar dari tiga";
+}
+return 0;
+}
